13f2b1f406846ec09d7d1d2b4f77104f0ded2fcf
NiketanOjha05 <niketanojha10@gmail.com>
2026-09-21 23:38:54 +0530

    insert

---
+++ ORSProject-04/src/main/java/in/co/rays/proj4/bean/SubjectBean.java
@@ -0,0 +1,38 @@
+package in.co.rays.proj4.bean;
+
+public class SubjectBean extends BaseBean {
+
+	private String name;
+	private String description;
+	private long courseId;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(long courseId) {
+		this.courseId = courseId;
+	}
+
+	@Override
+	public String getValue() {
+		return name;
+	}
+
+}
---
+++ ORSProject-04/src/main/java/in/co/rays/proj4/exception/DuplicateException.java
@@ -0,0 +1,9 @@
+package in.co.rays.proj4.exception;
+
+public class DuplicateException extends RuntimeException{
+	
+	public DuplicateException(String msg) {
+		super(msg);
+	}
+
+}
